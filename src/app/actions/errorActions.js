3f326af65eea7dc5b7d123de725fc9e6440442ed
Cho <richo@bestbuycanada.ca>
2018-02-04 15:37:23 -0800

    remove uneccessary thunk

--- src/app/actions/errorActions.js
+++ src/app/actions/errorActions.js
@@ -1,17 +1,15 @@
 import { errorActionTypes as types } from './actionTypes';
 
 export function errorOccured(error) {
-    return async (dispatch) => {
-        dispatch({
-            type: types.errorOccured,
-            message: error.message
-        });
-    }
+    return {
+        type: types.errorOccured,
+        message: error.message
+    };
 }
 
 export function clearError() {
     return {
-       type: types.clear,
-       message: ''
-    }
+        type: types.clear,
+        message: ''
+    };
 }
